c96da569564422d239982ef8c39f5e55934c5868
howItPhills <vlad-man001@mail.ru>
2022-02-06 14:59:22 +0300

    var and let scopes

---
+++ freecodecamp/varAndLetScope.js
@@ -0,0 +1,31 @@
+var printNumTwo;
+
+for (var i = 0; i < 4; i++) {
+   if (i === 2) {
+      printNumTwo = function () {
+         return i
+      }
+   }
+}
+
+console.log(printNumTwo()) //displays 4, not 2 cuz of var i = 0. The same would be if we assigned i outside of loop statement
+
+
+//
+for (let i = 0; i < 5; i++) {
+   if (i === 3) {
+      printNumTwo = function () {
+         return i
+      }
+   }
+}
+
+console.log(printNumTwo()) // displays 3, cuz let i = 0
+
+
+
+const array = [1, 2, 3]
+
+for (let i = 0; i < array.length; i++) {
+
+}
