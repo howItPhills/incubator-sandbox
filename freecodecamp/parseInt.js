d49de7ec1d7aebb930535bbcf7af1b0f2aec6378
howItPhills <vlad-man001@mail.ru>
2022-02-06 13:37:08 +0300

    parseInt + more of recursion

---
+++ freecodecamp/parseInt.js
@@ -0,0 +1,5 @@
+const parsing = (str) => {
+   return parseInt(str, 2)
+}
+
+console.log(parsing('101002001'))
--- freecodecamp/recursion.js
+++ freecodecamp/recursion.js
@@ -13,4 +13,34 @@ const multiply = (arr, n) => {
    }
 }
 
-console.log(multiply([1, 2, 3, 4], 4))
+// console.log(multiply([1, 2, 3, 4], 4))
+
+
+const countdown = (n) => {
+   if (n < 1) {
+      return []
+   } else {
+      const countArray = countdown(n - 1)
+      countArray.unshift(n)
+      return countArray
+   }
+}
+
+// console.log(countdown(10))
+
+
+const rangeOfNumbers = (startNum, endNum) => {
+   let rangeArray
+   if (startNum === endNum) {
+      return rangeArray = [startNum]
+   } else if (startNum < endNum) {
+      rangeArray = rangeOfNumbers(startNum, endNum - 1)
+      rangeArray.push(endNum)
+      return rangeArray
+   } else if (startNum > endNum) {
+      return "start number should be over end number"
+   }
+   return rangeArray
+}
+
+console.log(rangeOfNumbers(2, 5))
