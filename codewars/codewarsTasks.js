d0fdf577567635f4d7ab79d075b5094b344ee477
howItPhills <vlad-man001@mail.ru>
2022-02-27 16:18:29 +0300

    points

--- codewars/codewarsTasks.js
+++ codewars/codewarsTasks.js
@@ -18,4 +18,23 @@ const betterThanAverage = (classPoints, yourPoints) => {
    return !(Math.round(arr.reduce((acc, curr) => acc + curr) / arr.length) > yourPoints)
 }
 
-console.log(betterThanAverage([2, 3, 4], 1));
+// console.log(betterThanAverage([2, 3, 4], 1));
+
+const points = (games) => {
+   // let count = 0
+
+   // games.map(item => item.split(':')).forEach(item => {
+   //    if (item[0] > item[1]) {
+   //       count += 3
+   //    } else if (item[0] === item[1]) {
+   //       count += 1
+   //    }
+   // })
+
+   // return count
+   return games.reduce((acc, curr) => {
+      return acc += curr[0] > curr[2] ? 3 : curr[0] === curr[2] ? 1 : 0
+   }, 0)
+}
+
+console.log(points(['1:0', '2:0', '3:0', '4:0', '2:1', '3:1', '4:1', '3:2', '4:2', '4:3']));
